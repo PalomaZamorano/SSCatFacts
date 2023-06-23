9eed254dd163b78361bd0203e94e433c390fc308
Paloma Zamorano <pzamorano@buk.cl>
2023-06-22 20:03:37 -0400

    feat: Se agregan vistas correspondientes y modificaciones necesarias para login con username usando gema device

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:username]
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  def email_required?
+    false
+  end
 end
---
+++ db/migrate/20230622045036_remove_email_password_from_user.rb
@@ -0,0 +1,7 @@
+class RemoveEmailPasswordFromUser < ActiveRecord::Migration[7.0]
+
+    def change
+        remove_column :users, :email
+    end
+
+end
