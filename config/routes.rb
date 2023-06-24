52ec69a47ecbd4ca3b36cf65e2dd43762e8c1f01
Paloma Zamorano <pzamorano@buk.cl>
2023-06-24 17:15:53 -0400

    feat: Se agrega lógica para implementar tablas y favoritos en base a bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'fact_cats', to: 'fact_cats#index', as: 'fact_cats'
+  get 'fact_cats_favorite', to: 'fact_cats#favorite', as: 'fact_cats_favorite'
+  post 'fact_cats/:fact_cat_id/increment_click_count', to: 'fact_cats#increment_click_count', as: 'fact_cats_count_click'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
